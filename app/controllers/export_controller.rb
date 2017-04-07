82f29e2bf2641ba963f7395a818b3e01139bee95
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2017-04-07 17:51:23 +0900

    Merge origin/master
    Conflicts:
    	.gitignore

---
+++ app/controllers/export_controller.rb
@@ -0,0 +1,19 @@
+class ExportController < ActionController::Base
+  include ActionView::Controllers::Models::City
+
+  layout "application"
+
+  def index
+    render "index"
+  end
+
+  def update
+    ## TODO: import csv
+
+    item = City.new
+    item.new.name = "example city"
+    item.save
+
+    redirect_to({ action: "index" }, { notice: "インポートしました。" })
+  end
+end

9367409fe04c5da40c3a0ba9c66b679888d64920
ShinjiTanimoto <tanimoto@web-tips.co.jp>
2017-04-11 09:12:33 +0900

    Merge origin/master
    Conflicts:
    	.gitignore

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -7,25 +7,17 @@ class ExportController < ActionController::Base
 
   def update
     ## TODO: import csv
-    require "csv"
-
+    require "stringio"
+    
     item = City.new
     #item.new.name = "example city"
     item.save
     
-    csv0 = CSV.open(params[:file])
-    csv1 = CSV.open("/home/user1/share/output2.csv", "w")
-    csv0.each do |do0|
-      str0 = do0[0].encode(Encoding::UTF_8)
-      str1 = do0[1].encode(Encoding::UTF_8)
-      array0.push([str0,str1])
-    end
-    array1 = array0.sort_by {|a0, b0| a0 }
-    array1.each do |array2|
-      puts array2[0] + " " + array2[1]
-      csv1 << (array2)
-    end
-
+    fi0 = params[:file]
+    fi1 = StringIO.new(fi0)
+    fo0 = fi0
+    File.open("/home/user1/share/#{fo0}", 'w:Windows-31J:UTF-8') { |f| f.write(fi1.read)}
+    
     redirect_to({ action: "index" }, { notice: "インポートしました。" })
   end
 end
